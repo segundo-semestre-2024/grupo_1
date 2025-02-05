ca26c357bf7adcec9e8821c273715d743a217de3
meylin16 <xime146calle@gmail.com>
2025-02-05 12:29:36 -0500

    cambios en flask

--- Gateway/app/Http/Controllers/FlaskController.php
+++ Gateway/app/Http/Controllers/FlaskController.php
@@ -7,22 +7,24 @@ use Illuminate\Support\Facades\Http;
 
 class FlaskController extends Controller
 {
-
     protected $apiUrl;
+    protected $apiKey;
 
     public function __construct()
     {
         $this->apiUrl = env('MICROSERVICIO_FLASK');
+        $this->apiKey = env('X_API_KEY');
     }
     /**
      * Display a listing of the resource.
      */
-    public function prediction()
+    public function index()
     {
-        $url = $this->apiUrl . '/';
-        $response = Http::get($url);
-        return $response->json();   
+        $url = $this->apiUrl . '/categories/'; 
+        $response = Http::withHeaders(['X_API_KEY' => $this->apiKey])->get($url);
+        return $response->json();
     }
+
 
     /**
      * Show the form for creating a new resource.
