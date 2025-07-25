94516bef08bed8ecd9f4a9e8d85d516724942f36
nramirezoc <nramirezoc@unal.edu.co>
2025-07-25 03:15:10 -0500

    ayudaa

--- init.sql
+++ init.sql
@@ -1,5 +1,3 @@
--- Otorga todos los permisos al usuario 'user' desde cualquier host
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY 'userpassword';
-
--- Asegura que los cambios surtan efecto
+CREATE USER 'user'@'%' IDENTIFIED BY 'userpassword';
+GRANT ALL PRIVILEGES ON proyecto.* TO 'user'@'%';
 FLUSH PRIVILEGES;
